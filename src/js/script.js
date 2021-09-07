14548b4a853e801f9c1624fe941c52cd8be942ae
Eugene <evg.parkhomenko@gmail.com>
2021-09-07 23:41:08 +0300

    implement section .catalog, responses +adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -13,4 +13,22 @@ $(document).ready(function(){
       }
     ]
   });
+
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab--active)', function() {
+    $(this)
+      .addClass('catalog__tab--active').siblings().removeClass('catalog__tab--active')
+      .closest('div.container').find('ul.catalog__list').removeClass('catalog__list--active').eq($(this).index()).addClass('catalog__list--active');
+  });
+
+  toggleSlide = (item) => {
+    $(item).each(function(i) {
+      $(this).on('click', function(evt) {
+        evt.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content--active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list--active');
+      })
+    })
+  }
+  toggleSlide('.catalog-item__about');
+  toggleSlide('.catalog-item__back');
 });
